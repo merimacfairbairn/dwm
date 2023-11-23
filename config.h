fce5a9a6aa6baa45b886a68fe647ec767bc5d2b9
Larry <eirikrinnrauthr@tutanota.com>
2023-11-23 14:03:49 +0000

    Add keybinding to run slock

--- config.h
+++ config.h
@@ -65,11 +65,14 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *printscr[] = { "flameshot", "gui", NULL };
+static const char *lockscr[] = { "slock", "gui", NULL};
 
 static const Key keys[] = {
 	/* modifier						key		   function		   argument */
 	{ MODKEY,						XK_p,	   spawn,		   {.v = dmenucmd } },
 	{ MODKEY,       				XK_Return, spawn,		   {.v = termcmd } },
+    { 0,                            XK_Print,  spawn,          {.v = printscr } },
+    { MODKEY,             XK_Scroll_Lock,      spawn,          {.v = lockscr } },
 	{ MODKEY,						XK_b,	   togglebar,	   {0} },
 	{ MODKEY,						XK_j,	   focusstack,	   {.i = +1 } },
 	{ MODKEY,						XK_k,	   focusstack,	   {.i = -1 } },
@@ -102,7 +105,6 @@ static const Key keys[] = {
 	TAGKEYS(						XK_9,					   8)
 	{ MODKEY|ShiftMask,				XK_q,	    quit,		   {0} },
     { MODKEY|ControlMask|ShiftMask, XK_q,       quit,          {1} },
-    { 0,                            XK_Print,   spawn,         {.v = printscr } },
 };
 
 /* button definitions */
