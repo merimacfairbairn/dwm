2c0d032dbd613f03f68aa1e9eef12b7f4270d5b1
Larry <eirikrinnrauthr@tutanota.com>
2023-11-23 12:48:16 +0000

    Create a function that runs flameshot if XK_Print is pressed

--- config.h
+++ config.h
@@ -64,6 +64,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *printscr[] = { "flameshot", "gui", NULL };
 
 static const Key keys[] = {
 	/* modifier						key		   function		   argument */
@@ -99,8 +100,9 @@ static const Key keys[] = {
 	TAGKEYS(						XK_7,					   6)
 	TAGKEYS(						XK_8,					   7)
 	TAGKEYS(						XK_9,					   8)
-	{ MODKEY|ShiftMask,				XK_q,	   quit,		   {0} },
-    { MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} },
+	{ MODKEY|ShiftMask,				XK_q,	    quit,		   {0} },
+    { MODKEY|ControlMask|ShiftMask, XK_q,       quit,          {1} },
+    { 0,                            XK_Print,   spawn,         {.v = printscr } },
 };
 
 /* button definitions */
